a080121d249f96c208f28ff84d073d7b1939a58e
cblystone67 <christopherblystone1@gmail.com>
2023-06-08 22:00:44 -0700

    wip: deployment to heroku

--- tabbank/settings.py
+++ tabbank/settings.py
@@ -117,8 +117,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 LOGIN_REDIRECT_URL = '/songs/'
 LOGOUT_REDIRECT_URL = '/'
 STATIC_URL = 'static/'
