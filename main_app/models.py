6a8bbb2fbaef6b4f6e4bd96ec0b4199eea6bf798
cblystone67 <christopherblystone1@gmail.com>
2023-06-11 13:41:40 -0700

    Genre Model Added

--- main_app/models.py
+++ main_app/models.py
@@ -3,7 +3,6 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
-
 class Song(models.Model):
   title = models.CharField(max_length=50)
   artist = models.CharField(max_length=50)
@@ -19,4 +18,43 @@ class Song(models.Model):
   
   def get_absolute_url(self):
       return reverse("details", kwargs={"song_id": self.id})
+  
+class Genre(models.Model):
+  STYLES = (
+    ('ALT', 'Alternative'),
+    ('BLU', 'Blues'),
+    ('CLC', 'Classical'),
+    ('CNY', 'Country'),
+    ('EDM', 'Electronic Dance Music'),
+    ('HIP', 'Hip Hop'),
+    ('JAZ', 'Jazz'),
+    ('MTL', 'Metal'),
+    ('POP', 'Pop'),
+    ('RNB', 'Rhythm and Blues'),
+    ('RCK', 'Rock'),
+    ('REG', 'Reggae'),
+    ('IND', 'Indie'),
+    ('FOL', 'Folk'),
+    ('LAT', 'Latin'),
+    ('PUN', 'Punk'),
+    ('RAP', 'Rap'),
+    ('DSO', 'Disco'),
+    ('FUN', 'Funk'),
+    ('GOS', 'Gospel'),
+    ('RB', 'R&B'),
+    ('SOL', 'Soul'),
+    ('TEC', 'Techno'),
+    ('TRP', 'Trap'),
+  )
+  name = models.CharField(max_length=255)
+  songs = models.ManyToManyField(Song, related_name='genres')
+  style = models.CharField(
+    max_length=3,
+    choices=STYLES,
+    default=STYLES[0][0])
+  
+  song = models.ForeignKey(Song, on_delete=models.CASCADE, related_name='genre')
+  
+  def __str__(self) -> str:
+     return f"{self.get_style_display()} on {self.style}"
   
